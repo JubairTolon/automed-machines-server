6ba6f3f5c0a820b517590a54a6c7b74d16b7e263
ZubayerAhammedTolon <zs.tolon0073@gmail.com>
2022-09-05 19:48:51 +0600

    rating

--- index.js
+++ index.js
@@ -16,13 +16,85 @@ async function run() {
         await client.connect();
         const productCollection = client.db('automed-mechines').collection('products');
         const orderCollection = client.db('automed-mechines').collection('orders');
+        const messageCollection = client.db('automed-mechines').collection('messages');
+        const productReviewCollection = client.db('automed-mechines').collection('productReviews');
+
+
+        /************  All gets **************/
 
         //for store all products data
         app.get('/product', async (req, res) => {
             const query = {};
             const cursor = productCollection.find(query);
             const products = await cursor.toArray();
-            res.send(products)
+            // const cursorReview = productReviewCollection.find(query);
+            // const reviews = await cursorReview.toArray();
+            // const updatedProduct = products?.map(p => {
+            //     const sprs = reviews.filter(review => review.productId === p._id);
+            //     console.log(sprs)
+            //     let totalRating = 0;
+            //     sprs?.map(element => totalRating = totalRating + element.rating);
+            //     const rating = (Math.round(totalRating / sprs?.length));
+            //     // if (isNaN(rating)) {
+            //     //     p.rating = 1;
+            //     // }
+            //     // else {
+            //     //     p.rating = rating;
+            //     // }
+            //     return p
+            // })
+            res.send(products);
+        })
+        //for get all orders
+        app.get('/orders', async (req, res) => {
+            const query = {};
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders)
+        })
+        //get orders for specific users
+        app.get('/userOrders', async (req, res) => {
+            const userEmail = req.query.user; //here .user is query perameter to receive user email from clien site
+            const query = { user: userEmail };
+            const orders = await orderCollection.find(query).toArray();
+            res.send(orders);
+        })
+        //for get all messages to ui
+        app.get('/message', async (req, res) => {
+            const query = {};
+            const cursor = messageCollection.find(query);
+            const messages = await cursor.toArray();
+            res.send(messages)
+        })
+        //get message for specific users
+        app.get('/userMessage', async (req, res) => {
+            const userEmail = req.query.user; //here .user is query perameter to receive user email from clien site
+            const query = { user: userEmail };
+            const message = await messageCollection.find(query).toArray();
+            res.send(message);
+        })
+        app.get('/productReview', async (req, res) => {
+            const query = {};
+            const cursor = productReviewCollection.find(query);
+            const productReviews = await cursor.toArray();
+            res.send(productReviews)
+        })
+
+
+        /****************  All posts ******************/
+
+        //for add a product to product collection
+        app.post('/addProduct', async (req, res) => {
+            const product = req.body;
+            const result = await productCollection.insertOne(product);
+            res.send(result);
+        })
+
+        //for add a messages to message collection
+        app.post('/message', async (req, res) => {
+            const message = req.body;
+            const result = await messageCollection.insertOne(message);
+            res.send(result);
         })
 
         //for store orders for all users
@@ -32,12 +104,11 @@ async function run() {
             res.send(result);
         })
 
-        //get orders for specific users
-        app.get('/userOrders', async (req, res) => {
-            const userEmail = req.query.user; //here .user is query perameter to receive user email from clien site
-            const query = { user: userEmail };
-            const orders = await orderCollection.find(query).toArray();
-            res.send(orders);
+        //for store orders for all users
+        app.post('/productReview', async (req, res) => {
+            const productReview = req.body;
+            const result = await productReviewCollection.insertOne(productReview);
+            res.send(result);
         })
 
     }
